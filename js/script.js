b7f57b87386be161fbb6078eb05f6c6dd1f5bdb2
Brian Kidd <brian.kidd.one@gmail.com>
2018-09-19 13:37:56 -0700

    Pagination functional
    pagination is now functional. Next step is to add a search box

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@
 Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
-
 // Add variables that store DOM elements you will need to reference and/or manipulate
 let studentName = document.getElementsByClassName("student-item cf");
 let totalStudents = document.getElementsByClassName("student-item cf").length;
 const limitPerPage = 10;
-
+const allStudents = document.querySelectorAll(".student-item");//
+const studentSearch = document.querySelectorAll(".student-search");
+ 
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
 
@@ -27,135 +28,103 @@ for(let i = 0; i < list.length; i++){
 			list[i].style.display = "none";
 		}
 	}
+	
 }
-
-showPage(studentName, 1);
-
 
 // Create and append the pagination links - Creating a function that can do this is a good approach
 
-const appendPageLinks = (list) => {
-// if pagination already exists, remove it
+const appendPageLinks = list => {
+  // if pagination already exists, remove it
 
-// determine how many pages are needed for the list by dividing the total number of list items by the max number of items per page
-//let totalPages = Math.ceil(totalStudents/limitPerPage);
- let totalPages = Math.ceil(list.length / 10);
- alert("Total pages equals " + totalPages);
-//alert(totalPages);
+  // determine how many pages are needed for the list by dividing the total number of list items by the max number of items per page
+  //let totalPages = Math.ceil(totalStudents/limitPerPage);
+  let totalPages = Math.ceil(list.length / 10);
+  //alert(totalPages);
 
-// create a div, give it the “pagination” class, and append it to the .page div
-let pageDiv = document.querySelector(".page");
-let pageLinksDiv = document.createElement("div");//creation of the div
+  // create a div, give it the “pagination” class, and append it to the .page div
+  let pageDiv = document.querySelector('.page');
+  let pageLinksDiv = document.createElement('div'); //creation of the div
 
-pageLinksDiv.className = "pagination";//this is the class that the active class gets added to
-pageDiv.appendChild(pageLinksDiv);
+  pageLinksDiv.className = 'pagination'; //this is the class that the active class gets added to
+  pageDiv.appendChild(pageLinksDiv);
 
-let pageLinksUL = document.createElement("ul");
+  let pageLinksUL = document.createElement('ul');
 
-pageLinksUL.className = "pagination-ul";
-pageLinksDiv.appendChild(pageLinksUL);
+  pageLinksUL.className = 'pagination-ul';
+  pageLinksDiv.appendChild(pageLinksUL);
 
-// add a ul to the “pagination” div for every page
-for (let j = 1; j <= totalPages; j++) {
-    let pageLinkLI = document.createElement("li");
-    pageLinkLI.className = "pagination-li";
-    pageLinkLI.style.marginRight = "0.5em";
+  // add a ul to the “pagination” div for every page
+  for (let j = 1; j <= totalPages; j++) {
+	  console.log(j);
+    let pageLinkLI = document.createElement('li');
+    pageLinkLI.className = 'pagination-li';
+    pageLinkLI.style.marginRight = '0.5em';
     // create an anchor tag inside the li, and number it
-    let pageLinkA = document.createElement("a");
-	// add li and a tags with the page number text
-    pageLinkA.className = "inactive";
+    let pageLinkA = document.createElement('a');
+    // add li and a tags with the page number text	
+    pageLinkA.className = 'inactive';
     pageLinkA.textContent = j;
-    pageLinkA.style.cursor = "pointer";
+	if(pageLinkA.textContent === "1"){
+		window.onload = function(){
+			 pageLinkA.className = 'active';
+			
+		}
+	}
+    pageLinkA.style.cursor = 'pointer';
     pageLinkLI.appendChild(pageLinkA);
-	//let hello = alert ("hello");
+    //let hello = alert ("hello");
     // add the li to the parent ul
     pageLinksUL.appendChild(pageLinkLI);
   }
 
-// add an event listener to each a tag, or add an event listener to the pagination div,
-//let inactive = document.querySelectorAll(".inactive");
+  let paginationDiv = document.querySelector('.pagination'); //ok
+  let inactive = paginationDiv.getElementsByClassName('inactive'); //ok
+  //let active = document.querySelector(".active");
+  var active = paginationDiv.querySelectorAll('a');
+  active.className = 'active';
+ 
 
-
-
-//let active = 
-//const active = document.querySelector(".pagination-ul").querySelectorAll(".pagination-li").querySelectorAll("a");
-//.querySelectorAll(".pagination-li").querySelector("a");
-
-
- // set the class name of the <a> tag to "active" so the CSS will
-// display it in the "active" style
-	
-
-let paginationDiv = document.querySelector(".pagination");//ok
-let inactive = paginationDiv.getElementsByClassName("inactive");//ok
-//let active = document.querySelector(".active");
-var active = paginationDiv.querySelectorAll("a");
-active.className = "active";	
-//console.log(active);
-
-for (let i = 0; i < inactive.length; i++) {
-//inactive[i].classList.remove("active");	
-//let pageTab = inactive[i];
-console.log("the inactive array length is " + inactive.length);
-
-let r = 0;
-	while (r < inactive.length) {
-		inactive[r].classList.replace("active", "inactive");
-		r++;
-	}
-
-  inactive[i].addEventListener("click", function(e) {//ok
-  
-	   if(e.target && e.target.nodeName == "A"){
-			e.target.className = "active";
-			//pageTab.className = "active";
-			console.log(inactive[i]);
-			console.log("List item ", e.target.textContent, " was clicked!");
-	  } 
-/* 	  
+  for (let i = 0; i < inactive.length; i++) {
 	let r = 0;
-	while (r < inactive.length) {
-		inactive[r].classList.remove("active");
-		r++;
-	} */
-	console.log("the inactive array length is " + inactive.length);
-	/* for (let j = 0; j < active.length; j++) {
-		active[j].classList.remove("active");
-	}  */
-	  
-	pageLinksUL.addEventListener("click", event => {
-		if (event.target.className == "active") {
-		let pageClicked = event.target.textContent;
-		showPage(list, pageClicked);
+    inactive[i].classList.remove('active');
+    while (r < inactive.length) {
+      inactive[r].classList.replace('active', 'inactive');
+      r++;
+    }
+
+    inactive[i].addEventListener('click', function(event) {
+      if (event.target && event.target.nodeName == 'A') {
+        event.target.className = 'active';
+      }
+    });
+  }
+
+  pageLinksUL.addEventListener('click', event => {
+    document.querySelectorAll('a').forEach(element => {
+      if (element !== event.target) {
+        element.classList.remove('active');
+      }
+    });
+    if (event.target.className == 'active') {
+      showPage(list, event.target.textContent);
     }
   });
-	  
-	  
-	  
-  });
+
+};
+
+
+//sets up display for search capability
+function showSearchBox(list) {
+  
 }
 
- /* for (let j = 0; j < active.length; j++) {
-	active[j].classList.remove("active");
-}  */
-/* for (let j = 0; j < inactive.length; j++) {
-	inactive[j].classList.remove("active");
-} */
-
-
-//and use event delegation to target the a tags to define what happens they are clicked
-
-// calls the showPage function to display the appropriate page
-// loop over pagination links to remove active class from all
-// add the active class to the link that was just clicked, otherwise known as the
-//showPage(); 
-//event.target
-
+function searchStudents(list, name) {
 
 }
 
-//appendPageLinks(studentName, pageNumber );
+
 appendPageLinks(studentName);
+showPage(studentName, 1);
 
 
 
