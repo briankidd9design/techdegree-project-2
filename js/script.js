097c0d55731b263bbefea42279998ef6d30f1dd4
Brian Kidd <brian.kidd.one@gmail.com>
2018-10-03 19:40:47 -0700

    added smaller functions
    Added smaller more concise functions and resolved active class issue for the default value on the pagination links

--- js/script.js
+++ js/script.js
@@ -4,254 +4,217 @@ FSJS project 2 - List Filter and Pagination
 ******************************************/
 // Add variables that store DOM elements you will need to reference and/or manipulate
 //this creates an object with all the students in the list
-let allStudents = document.getElementsByClassName("student-item");
-//stores the length of the student-item array 
-let totalStudents = document.getElementsByClassName("student-item").length;
+/* const studentListUL = document.querySelectorAll('student-list li');
+console.log(studentListUL); */
+const allStudents = document.getElementsByClassName("student-item");
+const totalStudents = document.getElementsByClassName("student-item").length;
+const pageHeader = document.querySelector(".page-header");
+//document.getElementsByClassName(".student-list")[0].style.display = 'none';
+/***Setting Up the Search Input Field and also Search Button**/
+const searchInputField = document.createElement("div");
+	//adds the class name student-search to the searchInputField
+const searchStudentButton = document.createElement("button");
+const inputBox = document.createElement("input");//searchInput
 
-
+searchInputField.className = "student-search";
+	//finally appends the searchInputField to the page header class
+pageHeader.appendChild(searchInputField);
+inputBox.placeholder = "Search student list...";
+inputBox.className = "input-field";
+	//this adds the element input, the className input-field and the placeHolder text to the input box
+searchInputField.appendChild(inputBox);
+	//creation of the search button
+	//let searchStudentButton = document.createElement("button");
+searchStudentButton.textContent = "Search";
+searchInputField.appendChild(searchStudentButton);
+	//This event listener triggers the showSearchResults function and has list, userinput, and input for arguments
+	//to be used in the showSearchResults Function;
+/**************************************************************/
+	
 // Create a function to hide all of the items in the list excpet for the ten you want to show
 // Tip: Keep in mind that with a list of 54 studetns, the last page will only display four
-
-const showPage = (list, page) => {
+const showPage = (studentList, page) => {
 // loop over items in the list parameter
-for(let i = 0; i < list.length; i++){
+	for(let i = 0; i < studentList.length; i++){
 	//the bottom index number of the range of students to show on the page
-	let itemsPerPageFirst = (page * 10) - 10;
-	//the top index number of the range of students to show on the page
-	let itemsPerPageLast = (page * 10) - 1;
-	// if the index of a list item is >= the index of the first item that should be shown on
-	//the page, (index starts out 0 - 9 ))
-	//&& the list item index is <= the index of the last item that should be shown on page then show items
-	if (i >= itemsPerPageFirst && i <= itemsPerPageLast){
-			list[i].style.display = "block";
-		}// else hide items
-	else{
-			list[i].style.display = "none";
+	
+		let itemsPerPageFirst = (page * 10) - 10;
+		//the top index number of the range of students to show on the page
+		let itemsPerPageLast = (page * 10) - 1;
+		// if the index of a list item is >= the index of the first item that should be shown on
+		//the page, (index starts out 0 - 9 ))
+		//&& the list item index is <= the index of the last item that should be shown on page then show items
+		if (i >= itemsPerPageFirst && i <= itemsPerPageLast){
+				studentList[i].style.display = "block";
+			}// else hide items
+		else{
+				studentList[i].style.display = "none";
+			}
 		}
-	}
-	//this calls the showSearchBox function to create a search box and button on every page.
-	showSearchBox(allStudents);
 }
 
-// Create and append the pagination links - Creating a function that can do this is a good approach
+// Add functionality to the pagination buttons so that they show and hide the correct items
+const appendPageLinks = (studentList) => {
+  /***** Setting up the pagination links *****/
+// create a div, give it the “pagination” class, and append it to the .page div
+	let pageDiv = document.querySelector('.page');
+	let pageLinksDiv = document.createElement('div'); //creation of the div
+//this is the class that the active class gets added to
+	const pageLinksUL = document.createElement('ul');
+	pageLinksDiv.className = 'pagination'; 
+	pageDiv.appendChild(pageLinksDiv);
+//this creates the unorderd list of students to show per page
+//const pageLinksUL = document.createElement('ul');
+	pageLinksUL.className = 'pagination-ul';
+  //this appends the page links div with its class of pagination-ul to the pageLinksUL
+	pageLinksDiv.appendChild(pageLinksUL);
 
-const appendPageLinks = list => {
+	let paginationDiv = document.querySelector('.pagination'); //ok
+	let inactive = paginationDiv.getElementsByClassName('inactive'); //ok
+  //let active = document.querySelector(".active");
+	const active = paginationDiv.querySelectorAll('a');
+	active.className = 'active';	
+	
+/**************************************************/
   
 //total pages are rounded up from the total number of students divided by 10
-  let totalPages = Math.ceil(list.length / 10);
-
-  // create a div, give it the “pagination” class, and append it to the .page div
-  let pageDiv = document.querySelector('.page');
-  let pageLinksDiv = document.createElement('div'); //creation of the div
-//this is the class that the active class gets added to
-  pageLinksDiv.className = 'pagination'; 
-  pageDiv.appendChild(pageLinksDiv);
-//this creates the unorderd list of students to show per page
-  let pageLinksUL = document.createElement('ul');
-  pageLinksUL.className = 'pagination-ul';
-  //this appends the page links div with its class of pagination-ul to the pageLinksUL
-  pageLinksDiv.appendChild(pageLinksUL);
-
-  // add a ul to the “pagination” div for every page
-  for (let j = 1; j <= totalPages; j++) {
-	  console.log(j);
-    let pageLinkLI = document.createElement('li');
-    pageLinkLI.className = 'pagination-li';
-    pageLinkLI.style.marginRight = '0.5em';
-    // create an anchor tag inside the li, and number it
-    let pageLinkA = document.createElement('a');
-    // add li and a tags with the page number text	
-    pageLinkA.className = 'inactive';
-    pageLinkA.textContent = j;
-	//on window onload the class of the link with the textContent of 1 will be 'active'
-	if(pageLinkA.textContent === "1"){
-		window.onload = function(){
-			 pageLinkA.className = 'active';
-			
-		}
+	let totalPages = Math.ceil(studentList.length / 10);
+//this loop appends the pagination links adding the correct number taking into account the studentsList/10
+	for (let j = 1; j <= totalPages; j++) {
+	 // console.log(j); 
+		let pageLinkLI = document.createElement('li');//
+		pageLinkLI.className = 'pagination-li';
+		pageLinkLI.style.marginRight = '0.5em';
+		// create an anchor tag inside the li, and number it
+		let pageLinkA = document.createElement('a');
+		// add li and a tags with the page number text	
+		pageLinkA.className = 'inactive';
+		pageLinkA.textContent = j;
+		pageLinkA.style.cursor = 'pointer';
+		pageLinkLI.appendChild(pageLinkA);
+		// add the li to the parent ul
+		pageLinksUL.appendChild(pageLinkLI);
 	}
-    pageLinkA.style.cursor = 'pointer';
-    pageLinkLI.appendChild(pageLinkA);
-    // add the li to the parent ul
-    pageLinksUL.appendChild(pageLinkLI);
-  }
+	paginationEventListener(pageLinksUL, studentList);
+	addActiveClass(inactive);
+};
 
-  let paginationDiv = document.querySelector('.pagination'); //ok
-  let inactive = paginationDiv.getElementsByClassName('inactive'); //ok
-  //let active = document.querySelector(".active");
-  var active = paginationDiv.querySelectorAll('a');
-  active.className = 'active';
- 
-  for (let i = 0; i < inactive.length; i++) {
-	/* let r = 0;
-    inactive[i].classList.remove('active'); */
+//adding an event listener for the pagination links. 
+//changes page to that of the active class
+ const paginationEventListener = function (pageLinksList, studentList) {
 
-    inactive[i].addEventListener('click', function(event) {
-      if (event.target && event.target.nodeName == 'A') {
-        event.target.className = 'active';
-      }
-    });
-  }
-
-  pageLinksUL.addEventListener('click', event => {
+	//shows the correct page depending on if the link is active or not. If it is active it adds the link's textContent to the showPage() function
+	pageLinksList.addEventListener('click', event => {
     document.querySelectorAll('a').forEach(element => {
       if (element !== event.target) {
         element.classList.remove('active');
       }
     });
     if (event.target.className == 'active') {
-      showPage(list, event.target.textContent);
+      showPage(studentList, event.target.textContent);
     }
-  });
-
-};
-
-
-//sets up display for search capability
-//showSearchBox function must display search box and button and then also store the data entered into the search box 
-//in order to compare it with the student-name 
-function showSearchBox(list) {
-	//if a student search box exists then delete it to set up a new one
-	let duplicateSearchInputField = document.querySelector(".student-search");
+  }); 
 	
-	if(duplicateSearchInputField !== null) {
-		duplicateSearchInputField.parentNode.removeChild(duplicateSearchInputField);
+}; 
+//adds active class to pagination link that is clicked on
+ const addActiveClass = function(inactiveLink){
+	//console.log(inactiveLink);
+		for (let j = 0; j < inactiveLink.length; j++) {
+	
+		 inactiveLink[j].addEventListener('click', function(event) {
+		  if (event.target && event.target.nodeName == 'A') {
+			event.target.className = 'active';
+		  }
+		}); 
+	}		
+} 
+//this adds the active class to the first link of the pagination
+const setDefaultActiveClass = function(){
+	const pageLinksLI = document.querySelectorAll('.pagination-li A');
+	
+	for(let i = 0; i < pageLinksLI.length; i+=1){
+		if (i == 0){
+			pageLinksLI[i].className = "active";	 
+		}else{	 
+			pageLinksLI[i].className = "inactive";
+		}
+	} 
+}
+//event listener for searchStudentButton to invoke the showSearchResults function
+searchStudentButton.addEventListener("click", () => {
+	let userInput = inputBox.value.toLowerCase();
+	showSearchResults(allStudents, userInput, inputBox);
+});
+//keyUp event listener for searchInputField
+ searchInputField.addEventListener('keyup', (e) => {
+	let userInput = inputBox.value.toLowerCase();
+	showSearchResults(allStudents, userInput, inputBox);
+}); 
+	
+const studentsDisplayNone = function(){
+	let studentListClass = document.querySelectorAll('.student-list');
+	for(let i = 0; i < studentListClass.length; i++){
+		studentListClass[i].style.display = 'none';
 	}
 	
-	
-	let pageHeader = document.querySelector(".page-header");
-	//create a new div for the search field
-	let searchInputField = document.createElement("div");
-	//adds the class name student-search to the searchInputField
-	searchInputField.className = "student-search";
-	//finally appends the searchInputField to the page header class
-	pageHeader.appendChild(searchInputField);
-	
-	let input = document.createElement("input");
-	input.placeholder = "Search student list...";
-	input.className = "input-field";
-	//this adds the element input, the className input-field and the placeHolder text to the input box
-	searchInputField.appendChild(input);
-	//creation of the search button
-	let searchStudentButton = document.createElement("button");
-	searchStudentButton.textContent = "Search";
-	searchInputField.appendChild(searchStudentButton);
-	//This event listener triggers the showSearchResults function and has list, userinput, and input for arguments
-	//to be used in the showSearchResults Function;
-	searchStudentButton.addEventListener("click", () => {
-		let userInput = input.value.toLowerCase();
-		showSearchResults(list, userInput, input);
-		//console.log(userInput);
-
-	});
-	//KeyUp functionality
-	 let keyUpSearch = document.querySelector(".input-field");
-	 let studentListItem = document.getElementsByClassName("student-item");
-	 //reference for keyUp event listener: https://stackoverflow.com/questions/36897978/js-search-using-keyup-event-on-input-tag
-	 keyUpSearch.addEventListener("keyup", function() {
-		 Array.prototype.forEach.call(studentListItem, function(studentListItem){
-	/* 			 
-		let duplicatePaginationList = document.querySelector(".pagination-ul");
-	
-		if(duplicatePaginationList!== null) {
-			duplicatePaginationList.parentNode.removeChild(duplicatePaginationList);
-		} */
-			 	
-			 if(studentListItem.textContent.trim().indexOf(keyUpSearch.value) > -1){
-
-				 studentListItem.style.display = "block";
-				 //use appendPageLinke function to use the correct number of pagination links within the KeyUp event
-				 
-				 //let studentListItemArray = Array.from(studentListItem);//this converts the node list of all the students to an array
-				 
-				 
-				 // studentListItemArray.length;
-				// console.log("studentListItemArray equals " + studentListItemArray);
-			//	console.log(studentListItem);
-				
-				 
-			/* 	studentListItem.className = "student-keyup";
-				 let studentKeyUp = document.querySelector(".student-keyup");
-				 //appendPageLinks(studentKeyUp );
-				  */
-			 }
-			 else{
-				 studentListItem.style.display = "none";
-				
-			 }
-			 
-			  
-			 	
-		 });
-	 });
+}	
 	
 	
-}
-
-function showSearchResults(list, inputtedName, inputText) {//compare the list of students to the userInput
-	console.log(inputtedName);//user input
-	console.log(list);//the entire list of students with the class of ".studentItem"
-	//this converts the node list of all the students to an array
-	let listAsArray = Array.from(list);
-	//console.log(listAsArray);
 	
+function showSearchResults(studentList, userTextInput, inputTextBox) {//compare the list of students to the userInput
+	let duplicatePaginationList = document.querySelector(".pagination-ul");
+	let listAsArray = Array.from(studentList);
 	//The RegExp constructor creates a regular expression object for matching text with a pattern
 	//in this case it allows the user's search to be compared with letter values of the listAsArray array
-	let regExp = new RegExp(inputtedName);
+	let regExp = new RegExp(userTextInput);
 	//The filter() method creates a new array with all the elments that pass the test implemented by the provided function
-	
 	let matchesFoundArray = listAsArray.filter(text => regExp.test(text.innerText) === true);
-	 /* let matchesFoundArray = listAsArray.filter(function (text){
-			regExp.test(text.innerText) === true;
-		}); */
-	//console.log("matches " + matchesFoundArray );
-	 if (matchesFoundArray.length === 0){
-		 console.log("no results");
-		 inputText.value = "";
-		 //this changes the inputText from Search student list to No results for inputtedName
-		 inputText.placeholder = "No results for " + inputtedName;
+	if (matchesFoundArray.length === 0){
+		 //console.log("no results");
+		 inputTextBox.value = "";
+	
+		 //this changes the inputText from Search student list to No results for userInputText
+		 inputTextBox.placeholder = "No results for " + userTextInput;
+		 console.log("student list " + studentList);
 		 //if the user clicks in the inputbox then the page reloads 
-		 inputText.addEventListener("click", () =>{
-			 document.location.reload(true, showPage, list, 1);
+		 inputTextBox.addEventListener("click", () =>{
+			 document.location.reload(true, showPage, studentList, 1);
 		 });
-		 //inputText.placeholder.style.color = "red";
 		 //after now search results the page will reload after 5 seconds
-		let timeoutID = window.setTimeout(showPage, 5000, list, 1);
+		//let duplicatePaginationList = document.querySelector(".pagination-ul");
+		studentsDisplayNone();
+		duplicatePaginationList.style.display = "none";
+		 
+	setTimeout(
+	function() {
+	location.reload() },
+	1000 ); 
 	 } else {
-		 for (let i = 0; i < list.length; i+=1)
-			 list[i].style.display = "none";
+		  for (let i = 0; i < studentList.length; i+=1){
+			 studentList[i].style.display = "none";
+		 } 
 		 showPage(matchesFoundArray, 1);
 		//remove the pagination pages if they already exist after a search result is shown	
-		let duplicatePaginationList = document.querySelector(".pagination-ul");
+		//let duplicatePaginationList = document.querySelector(".pagination-ul");
 	
 		if(duplicatePaginationList!== null) {
 			duplicatePaginationList.parentNode.removeChild(duplicatePaginationList);
 		}
-		//set the first page link to active
-		//let anchor = document.getElementsByTagName("A");
 		
-		/* var hyperLinkText = document.querySelectorAll("a").innerHTML;
-		console.log("hyperlink text is " + hyperLinkText);
-		
-		if (hyperLinkText === "1"){
-			hyperLinkeText.className = "active";
-		} */
-		/* var x = document.querySelector(".inactive");
-		if (x.className = "inactive" && x.innerHTML === "1"){
-			x.className = "active";		
-		} */
+		let inactiveClass = document.getElementsByClassName('inactive');
 		 
-		appendPageLinks(matchesFoundArray); 	
+		appendPageLinks(matchesFoundArray); 
+		addActiveClass(inactiveClass);
+		setDefaultActiveClass();
 	}
-	 
 }
 
 appendPageLinks(allStudents);
 showPage(allStudents, 1);
+setDefaultActiveClass();
 
 
-// Add functionality to the pagination buttons so that they show and hide the correct items
-// Tip: If you created a function above to show/hide list items, it could be helpful here
+
 
 
 
